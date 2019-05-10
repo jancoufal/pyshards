c8e3976355dce04b6a82b0603d058dc05aeb5ce8
Jan Coufal <jcoufal@netsuite.com>
2019-05-10 16:06:09 +0200

    moveable and rotateable rectangle

--- pygame/moveable-rect.py
+++ pygame/moveable-rect.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 
 def main():
@@ -18,13 +19,15 @@ def main():
 	car_loc = OrientedPoint2D(
 		position=Vector2d(*screen_rect.center),
 		direction=Vector2d(0, 5),
+		initial_rotation=0,
+		rotation_angle_step=15,
 	)
 
 	key_down_event = {
 		pygame.K_UP: OrientedPoint2D.forward,
 		pygame.K_DOWN: OrientedPoint2D.backward,
-		# pygame.K_LEFT: lambda pos: (pos[0] - car_speed, pos[1]),
-		# pygame.K_RIGHT: lambda pos: (pos[0] + car_speed, pos[1]),
+		pygame.K_LEFT: OrientedPoint2D.turn_left,
+		pygame.K_RIGHT: OrientedPoint2D.turn_right,
 	}
 
 	print(key_down_event)
@@ -32,11 +35,13 @@ def main():
 	while True:
 		for event in pygame.event.get():
 
-			screen.blit(car_surf, car_loc.position.xy)
+			car_rot = pygame.transform.rotate(car_surf, car_loc.rotation_angle)
+			screen.blit(car_rot, car_loc.position.xy)
 
 			pygame.display.update()
 
-			print(event)
+			# print(event)
+			print(car_loc)
 
 			if event.type == pygame.KEYDOWN and event.key in key_down_event.keys():
 				key_down_event[event.key](car_loc)
@@ -48,6 +53,9 @@ def main():
 class Vector2d(object):
 	def __init__(self, x, y):
 		self._x, self._y = x, y
+
+	def __str__(self):
+		return f"vec({self.x, self.y})"
 
 	@property
 	def x(self):
@@ -67,11 +75,25 @@ class Vector2d(object):
 	def __sub__(self, other):
 		return Vector2d(self.x - other.x, self.y - other.y)
 
+	@classmethod
+	def rotate(cls, vector2d, degrees):
+		x, y, r = vector2d.x, vector2d.y, math.radians(degrees)
+		return cls(
+			x * math.cos(r) + y * math.sin(r),
+			-x * math.sin(r) + y * math.cos(r)
+		)
+
 
 class OrientedPoint2D(object):
-	def __init__(self, position=Vector2d(0, 0), direction=Vector2d(1, 0)):
+	def __init__(self, position=Vector2d(0, 0), direction=Vector2d(1, 0), initial_rotation=0, rotation_angle_step=5):
 		self._pos = position
-		self._dir = direction
+		self._dir_initial = direction
+		self._dir_actual = direction
+		self._angle_step = rotation_angle_step
+		self._angle_actual = initial_rotation
+
+	def __str__(self):
+		return f"orientedPoint2D(pos => {self.position}, dir => {self.direction}, angle => {self.rotation_angle})"
 
 	@property
 	def position(self):
@@ -79,13 +101,28 @@ class OrientedPoint2D(object):
 
 	@property
 	def direction(self):
-		return self._dir
+		return self._dir_actual
+
+	@property
+	def rotation_angle(self):
+		return self._angle_actual
 
 	def forward(self):
-		self._pos += self._dir
+		self._pos += self.direction
 
 	def backward(self):
-		self._pos -= self._dir
+		self._pos -= self.direction
+
+	def turn_left(self):
+		self._rotate(self._angle_step)
+
+	def turn_right(self):
+		self._rotate(-self._angle_step)
+
+	def _rotate(self, step):
+		self._angle_actual += step
+		self._angle_actual %= 360
+		self._dir_actual = Vector2d.rotate(self._dir_initial, self._angle_actual)
 
 
 if __name__ == "__main__":
