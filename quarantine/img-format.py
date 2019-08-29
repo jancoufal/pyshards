9867557d09327bbad0dac102d27b630c88e0f0c5
Jan Coufal <jcoufal@netsuite.com>
2019-08-29 17:30:25 +0200

    better image viewer

--- quarantine/img-format.py
+++ quarantine/img-format.py
@@ -2,55 +2,24 @@
 
 import glob
 from pathlib import Path
+import struct
 import pygame
 import time
 
 
 def main():
-	files = sorted(glob.glob("data/images/*.img"))
+	img_files = sorted(glob.glob("data/images/*.img"))
 
-	scan_heads(files)
-	exit(2)
+	# preload images
+	imgs = [ImageX(f) for f in img_files]
+
+	mode_13h_zoom = 4
 
 	pygame.init()
 	pygame.display.set_caption("quarantine pix format reconstruction")
-	screen_rect = pygame.Rect(0, 0, 320, 200)
-	screen = pygame.display.set_mode(screen_rect.size)
+	screen = pygame.display.set_mode(Mode13h.as_rect(mode_13h_zoom).size)
 
-	for f in files:
-		content = Path(f).read_bytes()
-		print(f"filename: {f}")
-		print(f"    size: {len(content)}")
-		print(f"    head: {content[0:6].decode('ansi')}")
-		width = int.from_bytes(content[6:8], byteorder='little')
-		print(f"   width: {width}")
-		height = int.from_bytes(content[8:10], byteorder='little')
-		print(f"  height: {height}")
-		print(f"  pixel count: {width * height}")
-		img_data = content[10:]
-		print(f"  data size: {len(img_data)}")
-		print("drawing...")
-
-		img_surface = pygame.Surface((width, height), flags=0, depth=32)
-
-		pix_array = pygame.PixelArray(img_surface)
-		pix_idx = 0
-		for b in img_data:
-			pix_array[pix_pos(pix_idx)] = (
-				map_to_range((b >> 4) & 0x03, 0, 0x3, 0, 0xff),
-				map_to_range((b >> 2) & 0x03, 0, 0x3, 0, 0xff),
-				map_to_range((b >> 0) & 0x03, 0, 0x3, 0, 0xff)
-			)
-			pix_idx += 1
-		pix_array.close()
-
-		screen.fill(pygame.color.Color("#00000000"))
-		screen.blit(img_surface, img_surface.get_rect())
-		pygame.display.flip()
-
-		time.sleep(2)
-
-	print("image loop ends...")
+	img_idx = 0
 
 	done = False
 	while not done:
@@ -58,33 +27,76 @@ def main():
 			if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.unicode == 'q'):
 				done = True
 
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					img_idx -= 1
+				if event.key == pygame.K_RIGHT:
+					img_idx += 1
 
-def pix_pos(idx):
-	x, y = idx % 320, idx // 320
-	if y >= 200:
-		raise RuntimeError("index out of range!")
-	return x, y
+				img_idx %= len(imgs)
+				imgx = imgs[img_idx]
 
+				pygame.display.set_caption(str(imgx))
 
-def map_to_range(val, val_min, val_max, range_min, range_max):
-	src_range = val_max - val_min
-	dst_range = range_max - range_min
-	src_val = val - val_min		# normalization
-	src_pos = src_val / src_range
-	return int(range_min + src_pos * dst_range)
+				scaled_surface = pygame.transform.scale(imgx.surface, Mode13h.as_rect(mode_13h_zoom).size)
 
-
-def scan_heads(files):
-	for f in files:
-		content = Path(f).read_bytes()
-		print(f"{f[12:]:12s}", end=": ")
-		print(bytes(map(byte_val_to_printable_ansi_byte, content[10:800])).decode(encoding="ansi"))
+				screen.fill(pygame.color.Color("#00000000"))
+				screen.blit(scaled_surface, Mode13h.as_rect(mode_13h_zoom))
+				pygame.display.flip()
 
 
-def byte_val_to_printable_ansi_byte(b):
-	if b in (0, ascii('\n'), ascii('\r')):
-		return ord('.')
-	return b
+class Mode13h(object):
+	WIDTH = 320
+	HEIGHT = 200
+
+	@staticmethod
+	def index_to_xy(index):
+		x, y = index % Mode13h.WIDTH, index // Mode13h.WIDTH
+		if y >= Mode13h.HEIGHT:
+			raise RuntimeError(f"Index {index} out of range (=> [{x}:{y}])!")
+		return x, y
+
+	@staticmethod
+	def as_rect(mult_factor=1):
+		return pygame.Rect(0, 0, mult_factor * Mode13h.WIDTH, mult_factor * Mode13h.HEIGHT)
+
+	@staticmethod
+	def map_range_value_to_range(src_val, src_min, src_max, dst_min, dst_max):
+		src_range = src_max - src_min
+		dst_range = dst_max - dst_min
+		norm_val = src_val - src_min  # normalization
+		src_percent = norm_val / src_range
+		return int(dst_min + src_percent * dst_range)
+
+
+class ImageX(object):
+
+	STRUCT_FMT_HEAD = "6sHH"
+
+	def __init__(self, filename):
+		self.filename = filename
+		self.raw_data = Path(self.filename).read_bytes()
+		head_struct = struct.unpack_from(ImageX.STRUCT_FMT_HEAD, self.raw_data)
+		self.imagex = head_struct[0]
+		self.width = head_struct[1]
+		self.height = head_struct[2]
+		self.surface = pygame.Surface((self.width, self.height), flags=0, depth=32)
+		self._fill_surface()
+
+	def _fill_surface(self):
+		pix_array = pygame.PixelArray(self.surface)
+
+		offset = struct.calcsize(ImageX.STRUCT_FMT_HEAD)
+		pix_idx = 0
+		for s in struct.iter_unpack("B", self.raw_data[offset:]):
+			b = s[0]
+			clr_idx = Mode13h.map_range_value_to_range(b, 0x0, 0xff, 0x0, 0xff)
+			pix_array[Mode13h.index_to_xy(pix_idx)] = (clr_idx, clr_idx, clr_idx)
+			pix_idx += 1
+		pix_array.close()
+
+	def __str__(self):
+		return f"{self.filename} > {self.width} x {self.height}"
 
 
 if __name__ == "__main__":
