e1dcaf5efa75beec8f3db01d7079a0fa4972fc95
Jan Coufal <jcoufal@netsuite.com>
2020-03-06 10:17:40 +0100

    opengl - fragment shader test

--- opengl/fragment-shader-test.py
+++ opengl/fragment-shader-test.py
@@ -17,7 +17,8 @@ def create_shader(shader_type, source):
 	glShaderSource(shader, source)
 	glCompileShader(shader)
 
-	shaders.compileShader(source, shader_type)
+	# for error hunting
+	# shaders.compileShader(source, shader_type)
 
 	return shader
 
@@ -35,7 +36,7 @@ def timer(value):
 
 def draw():
 	glClear(GL_COLOR_BUFFER_BIT)
-	glUniform1f(timeLocation,time)
+	glUniform1f(timeLocation, time)
 	glRecti(-1, -1, 1, 1)
 	glutSwapBuffers()
 
@@ -51,40 +52,10 @@ def main():
 	glutCreateWindow(b"PyOpenGL")
 	glutDisplayFunc(draw)
 	glutIdleFunc(draw)
-	fragment = create_shader(GL_FRAGMENT_SHADER, """
-uniform float time;
-uniform vec2 resolution;
-// based on https://www.shadertoy.com/view/MtBGDW
-#define FIELD 5.0
-#define CHANNEL bvec3(true, true, true)
-#define TONE vec3(0.299,0.587,0.114)
 
-vec2 pieuvreEQ(vec3 p,float t){
-	vec2 fx = p.xy;
-	fx.x = (fx.y+length(p*fx.x)-cos(t+fx.y));
-	fx.x = (fx.y+length(p*fx.x)-cos(t+fx.y));
-	fx.x = (fx.y+length(p*fx.x)-cos(t+fx.y));
-	fx.x*=fx.x*0.1;
-	return fx;}
+	with open("fragment-shader-test.shader", "r") as fh:
+		fragment = create_shader(GL_FRAGMENT_SHADER, fh.read())
 
-vec3 computeColor(vec2 fx) {
-	vec3 color = vec3(CHANNEL) * TONE;
-	color -= fx.x;
-	color.b += color.g*1.5;
-	return clamp(color,0.0,1.0);
-}
-
-void main(void) {
-	float ratio = resolution.y / resolution.x;
-	gl_FragCoord.y *= ratio;
-	vec2 position = (gl_FragCoord.xy / resolution.xy) - vec2(0.5, 0.6 * ratio);
-	vec3 p = position.xyx * FIELD;
-	vec3 color = vec3(0.0, 0.0, 0.2);
-	
-	color += computeColor(pieuvreEQ(p * 2.5, time));
-	gl_FragColor = vec4(color, 1.0);
-}
-	""")
 	program = glCreateProgram()
 	glAttachShader(program, fragment)
 	glLinkProgram(program)
---
+++ opengl/fragment-shader-test.shader
@@ -0,0 +1,37 @@
+uniform float time;
+uniform vec2 resolution;
+// based on https://www.shadertoy.com/view/MtBGDW
+#define FIELD 5.0
+#define CHANNEL bvec3(true, true, true)
+#define TONE vec3(0.299, 0.587, 0.114)
+
+vec2 pieuvreEQ(vec3 p,float t)
+{
+	vec2 fx = p.xy;
+	fx.x = fx.y - cos(fx.y);
+//	fx.x = (fx.y+length(p*fx.x)-cos(t+fx.y));
+//	fx.x = (fx.y+length(p*fx.x)-cos(t+fx.y));
+//	fx.x = (fx.y+length(p*fx.x)-cos(t+fx.y));
+	fx.x*=fx.x * 0.1;
+	return fx;
+}
+
+vec3 computeColor(vec2 fx)
+{
+	vec3 color = vec3(CHANNEL) * TONE;
+	color -= fx.x;
+	color.b += color.g * 1.5;
+	return clamp(color, 0.0, 1.0);
+}
+
+void main(void)
+{
+	float ratio = resolution.y / resolution.x;
+	gl_FragCoord.y *= ratio;
+	vec2 position = (gl_FragCoord.xy / resolution.xy) - vec2(0.5, 0.6 * ratio);
+	vec3 p = position.xyx * FIELD;
+	vec3 color = vec3(0.0, 0.0, 0.2);
+
+	color += computeColor(pieuvreEQ(p * 2.5, time));
+	gl_FragColor = vec4(color, 1.0);
+}
