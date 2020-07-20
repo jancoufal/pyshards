d57c1749c9571cb37de4ca2f12588a059e5fee8b
Jan Coufal <jan.coufal@gmail.com>
2020-07-20 16:45:46 +0200

    ogl-funcs.py - HandlerBase

--- ogl-funcs.py
+++ ogl-funcs.py
@@ -2,16 +2,22 @@ import xml.sax as sax
 
 
 def main():
-	h = GlXmlHandler()
-	sax.parse("gl.xml", h, errorHandler=None)
-	print(str(h))
+	# handler_definitions = {
+	# 	"registry": HandlerRegistry,
+	# 	"registry>types": HandlerTypes,
+	# 	"registry>types>type": HandlerType,
+	# }
+
+	# g = HandlerGlXml.create(handler_definitions)
+	g = HandlerStructScanner.create()
+	sax.parse("gl.xml", handler=SaxXmlHandler(g), errorHandler=None)
+	print(str(g))
 
 
-class GlXmlHandler(sax.ContentHandler):
-	def __init__(self):
-		self._handler = StructScanner(None)
-		# self._handler = NoOpHandler(None)
-		# self._handler = HandlerRoot(None)
+class SaxXmlHandler(sax.ContentHandler):
+	def __init__(self, handler):
+		self._handler = handler
+		# self._handler = StructScanner(None)
 		self._data = ""
 
 	def startElement(self, tag, attributes):
@@ -26,58 +32,101 @@ class GlXmlHandler(sax.ContentHandler):
 
 
 class HandlerBase(object):
-	def __init__(self, parent_handler):
+	def __init__(self, parent_handler, tag, attributes):
 		self._parent = parent_handler
+		self.tag = tag
+		self.attributes = attributes
+		self.data = None
 
 	def get_handler(self, tag, attributes):
-		return self._get_handler(tag, attributes)
-
-	def _get_handler(self, tag, attributes):
 		raise NotImplementedError
 
-	def finish(self, tag, content):
-		self._parent._accept(tag, self._on_content(content))
+	def finish(self, tag, data):
+		if tag != self.tag:
+			raise AssertionError(f"Tags does not match ({self.tag}, {tag}")
+
+		self.data = data
+		self._parent.on_child_finish(self)
 		return self._parent
 
-	def _on_content(self, content):
-		return content
-
-	def _accept(self, tag, content):
+	def on_child_finish(self, child):
 		raise NotImplementedError
 
 
-class StructScanner(HandlerBase):
-	def __init__(self, parent_handler):
-		super(StructScanner, self).__init__(parent_handler)
-		self._structure = dict()
+class HandlerStructScanner(HandlerBase):
+	@classmethod
+	def create(cls):
+		return cls(None, None, None)
+
+	def __init__(self, parent_handler, tag, attributes):
+		super(HandlerStructScanner, self).__init__(parent_handler, tag, attributes)
+		self._paths = dict()
 
 	def __str__(self):
-		return str(self._structure)
+		return str(self._paths)
 
-	def _get_handler(self, tag, att):
-		self._structure[tag] = dict()
-		return StructScanner(self)
+	def get_handler(self, tag, attributes):
+		return HandlerStructScanner(self, tag, attributes)
 
-	def _on_content(self, content):
-		return self._structure
+	def on_child_finish(self, child):
+		self._accumulate_path(child.tag)
+
+	def _accumulate_path(self, child_tag_path):
+		if self._parent is not None:
+			p = self.tag + ">" + child_tag_path
+			self._parent._accumulate_path(p)
+		else:
+			self._paths.setdefault(child_tag_path, 0)
+			self._paths[child_tag_path] += 1
+
+
+class HandlerNoOp(HandlerBase):
+	def _accept(self, tag, content):
+		pass
+
+
+"""
+class HandlerGlXml(HandlerBase):
+	@classmethod
+	def create(cls, handler_definitions):
+		return cls(None, None, None)
+
+	def _get_handler_factory(self):
+		return {
+			"registry": HandlerRegistry,
+		}
 
 	def _accept(self, tag, content):
-		self._structure[tag].update(content)
+		print(f"HandlerGlXml > {tag=}, {content=}")
 
 
-'''
-class NoOpHandler(object):
-	def __init__(self, ctx):
-		self._ctx = ctx
+class HandlerRegistry(HandlerBase):
+	def _get_handler_factory(self):
+		return {
+			"types": HandlerTypes,
+		}
 
-	def get_handler(self, tag, attr):
-		return NoOpHandler(self)
+	def _accept(self, tag, content):
+		print(f"HandlerRegistry > {tag=}, {content=}")
 
-	def finish(self, tag, content):
-		return self._ctx
 
-	def accept(self, *args, **kwargs):
-		pass
+class HandlerTypes(HandlerBase):
+	def _get_handler_factory(self):
+		return {
+			"type": HandlerType,
+		}
+
+	def _accept(self, tag, content):
+		print(f" HandlerTypes > {tag=}, {content=}")
+
+
+class HandlerType(HandlerBase):
+	def _get_handler_factory(self):
+		return {
+		}
+
+	def _accept(self, tag, content):
+		print(f"  HandlerType > {tag=}, {content=}")
 
 
 class HandlerRoot(object):
@@ -196,7 +245,7 @@ class GlFunction(object):
 
 	def __str__(self):
 		return f"{self._ret_type} {self._fn_name}"
-'''
+"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
