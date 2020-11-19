884efc00b5258e62e8d996c9057cd0a3daeb2b8d
Jan Coufal <jan.coufal@gmail.com>
2020-11-19 17:01:08 +0100

    code fixes + exceptional result object

--- web-scrappers/scrappers/impl/roumen.py
+++ web-scrappers/scrappers/impl/roumen.py
@@ -5,7 +5,7 @@ import sqlite3
 from ._base import Base
 from ..sources import Source
 from ..settings import Settings
-from ..result import Result
+from ..result import Result, ExceptionResultItem
 
 
 class _RoumenSettings(object):
@@ -13,7 +13,7 @@ class _RoumenSettings(object):
 		self.base_url = base_url
 		self.base_url_params = base_url_params
 		self.img_base = img_base
-		self.href_needle = hre
+		self.href_needle = href_needle
 
 
 class BaseRoumen(Base):
@@ -24,7 +24,7 @@ class BaseRoumen(Base):
 
 	def __init__(self, source: Source, settings: Settings, roumen_settings: _RoumenSettings):
 		super().__init__(source, settings)
-		self._roumen_settins = roumen_settings
+		self._roumen_settings = roumen_settings
 
 	def scrap(self):
 		ts = datetime.datetime.now()
@@ -40,7 +40,7 @@ class BaseRoumen(Base):
 					destination_path.mkdir(parents=True, exist_ok=True)
 					relative_file_path = relative_path / image_to_download
 
-					remote_file_url = f"{self._img_base}/{image_to_download}"
+					remote_file_url = f"{self._roumen_settings.img_base}/{image_to_download}"
 					# r = requests.get(remote_file_url, headers=Roumen.REQUEST_HEADERS)
 					urllib.request.urlretrieve(remote_file_url, filename=str(destination_path / image_to_download))
 
@@ -50,14 +50,38 @@ class BaseRoumen(Base):
 					result.on_item_success(relative_file_path, remote_file_url)
 
 				except:
-					result.on_item_failure(image_to_download, "\n".join(traceback.format_exception(*sys.exc_info())))
+					result.on_item_failure(image_to_download, ExceptionResultItem.createFromLastException())
 
 			result.on_scrapping_finished()
 
 		except:
-			result.on_scrapping_exception("\n".join(traceback.format_exception(*sys.exc_info())))
+			result.on_scrapping_exception(ExceptionResultItem.createFromLastException())
 
 		return result
+
+	def _get_images_to_download(self):
+		remote_image_names = self._scrap_image_names()
+		stored_image_names = self.read_last_images_from_db()
+		images_to_download = [name for name in remote_image_names if name not in stored_image_names]
+
+		# remove possible duplicates (and preserve order)
+		seen = set()
+		seen_add = seen.add
+		return reversed([_ for _ in images_to_download if not (_ in seen or seen_add(_))])
+
+	def _scrap_image_names(self):
+		r = requests.get(self._roumen_settings.base_url, params=self._roumen_settings.base_url_params)
+		soup = bs4.BeautifulSoup(r.content.decode(r.apparent_encoding), features="html.parser")
+
+		# extract all "a" tags having "roumingShow.php" present in the "href"
+		all_urls = map(lambda a: urllib.parse.urlparse(a.get("href")), soup.find_all("a"))
+		all_show = [url for url in all_urls if self._roumen_settings.href_needle in url.path]
+
+		# extract all "file" values from the query string
+		all_qstr = [urllib.parse.parse_qs(url.query) for url in all_show]
+		all_imgs = [qs.get("file").pop() for qs in all_qstr if "file" in qs]
+
+		return all_imgs
 
 
 class Roumen(BaseRoumen):
@@ -73,7 +97,7 @@ class RoumenMaso(BaseRoumen):
 	def __init__(self, settings: Settings):
 		super().__init__(Source.ROUMEN, settings, _RoumenSettings(
 			base_url="https://www.roumenovomaso.cz",
-			base_url_params={ "agree": "on" },
-			img_base="https://www.roumenovomaso.cz/upload,
+			base_url_params={"agree":"on"},
+			img_base="https://www.roumenovomaso.cz/upload",
 			href_needle="masoShow.php"
 		))
--- web-scrappers/scrappers/result.py
+++ web-scrappers/scrappers/result.py
@@ -1,8 +1,36 @@
+import sys, traceback
 import typing
 import pathlib
 import datetime
 from .sources import Source
 
+
+class ExceptionResultItem(object):
+	@classmethod
+	def createFromLastException(cls):
+		return cls(*sys.exc_info())
+
+	def __init__(self, exception_type, value, traceback):
+		self._exception_type = exception_type
+		self._value = value
+		self._traceback = traceback
+
+	@property
+	def exception_type(self):
+		return self._exception_type
+
+	@property
+	def value(self):
+		return self._value
+
+	@property
+	def traceback(self):
+		return self._traceback
+
+	@property
+	def formatted_exception(self):
+		return traceback.format_exception(etype=self.exception_type, value=self.value, tb=self.traceback)
+		
 
 class Result(object):
 	@classmethod
@@ -16,7 +44,7 @@ class Result(object):
 		self._items_suceeded = 0
 		self._items_failed = 0
 		self._errors = list()
-		self._general_error = None
+		self._general_error = list()
 
 	def __str__(self):
 		return f"Result of [{self._source.value}] scrapper: {self.items_suceeded} of {self.items_total} ({self.suceeded_percentage_str}) scrapped in {self.time_taken}"
@@ -24,15 +52,15 @@ class Result(object):
 	def on_item_success(self, local_file: pathlib.Path, url: str):
 		self._items_suceeded += 1
 
-	def on_item_failure(self, item_name: str, error_message: str):
+	def on_item_failure(self, item_name: str, error: ExceptionResultItem):
 		self._items_failed += 1
-		self._errors.append((item_name, error_message))
+		self._errors.append((item_name, error))
 
 	def on_scrapping_finished(self):
 		self._time_taken = str(datetime.datetime.now() - self._ts_start)
 
-	def on_scrapping_exception(self, error_message: str):
-		self._general_error = error_message
+	def on_scrapping_exception(self, error: ExceptionResultItem):
+		self._general_error = error
 		self.on_scrapping_finished()
 
 	@property
@@ -56,9 +84,9 @@ class Result(object):
 		return "n/a" if self.items_total == 0 else f"{(100.0 * self.items_suceeded) / self.items_total:3.2f}%"
 
 	@property
-	def errors(self):
+	def errors_list(self):
 		return self._errors
 
 	@property
-	def general_error(self):
+	def general_error_list(self):
 		return self._general_error
