3f3e69009982822cd2d503e4fcbf00772631353e
Jan Coufal <jan.coufal@gmail.com>
2023-03-14 13:44:09 +0100

    ogl-terrain-grid.py

--- ogl-terrain-grid.py
+++ ogl-terrain-grid.py
@@ -1,20 +1,49 @@
 from dataclasses import dataclass
+from enum import Enum, auto
+from typing import List, Tuple
 from itertools import takewhile
+from functools import reduce
+from operator import add
 import pygame
 
-@dataclass
-class Point():
-	x: int
-	y: int
 
-	def as_tuple(self): return (self.x, self.y)
+GL_INDEX_RESET = -1
+
+
+class DrawMode(Enum):
+	TRIANGLE_STRIP_WITH_RESET_POINT = auto(),
+	SEPARATE_TRIANGLES = auto()
+
+
+@dataclass
+class Point:
+	x: float
+	y: float
+
+	def as_tuple(self): return self.x, self.y
 	def __add__(self, other): return Point(self.x + other.x, self.y + other.y)
-	def __mul__(self, other): return Point(self.x * other.x, self.y * other.y)
-	def __truediv__(self, other): return Point(self.x / other.x, self.y / other.y)
+	def __sub__(self, other): return Point(self.x - other.x, self.y - other.y) if isinstance(other, Point) else Point(self.x - other, self.y - other)
+	def __mul__(self, other): return Point(self.x * other.x, self.y * other.y) if isinstance(other, Point) else Point(self.x * other, self.y * other)
+	def __truediv__(self, other): return Point(self.x / other.x, self.y / other.y) if isinstance(other, Point) else Point(self.x / other, self.y / other)
 	def __repr__(self): return f"[{self.x},{self.y}]"
 
 
-def generatePlane(screen, line_fractions: Point, edge_length: Point, starting_point: Point):
+@dataclass
+class Triangle:
+	a: Point
+	b: Point
+	c: Point
+
+	def as_points_closed(self):
+		return self.a, self.b, self.c, self.a
+
+	def scale(self, factor: float):
+		v = self.a, self.b, self.c
+		center = reduce(add, v) / len(v)
+		return Triangle(*map(lambda w: center + (w - center) * factor, v))
+
+
+def generate_plane(screen, line_fractions: Point, edge_length: Point, starting_point: Point, draw_mode: DrawMode):
 	print(f"{line_fractions=}")
 	print(f"{edge_length=}")
 	print(f"{starting_point=}")
@@ -26,22 +55,25 @@ def generatePlane(screen, line_fractions: Point, edge_length: Point, starting_po
 	print(f"{delta=}")
 
 	vertices = []
-	for x in range(line_fractions.x + 1):
-		for y in range(line_fractions.y + 1):
+	for x in range(int(line_fractions.x) + 1):
+		for y in range(int(line_fractions.y) + 1):
 			vertices.append(starting_point + delta * Point(x, y))
 	print(f"{vertices=}")
 	print(f"{len(vertices)=}")
 
-	print("index mode: triangle strip with reset point")
-	index_count = (line_fractions.y + 1) * line_fractions.x * 2 + line_fractions.x
-	print(f"{index_count=}")
+	print(f"draw mode: {draw_mode}")
 	indices = []
-	reset_index = -1
-	for x in range(line_fractions.x):
-		for y in range(line_fractions.y + 1):
-			indices.append((x + 0) * line_fractions.y + y + x + 0)
-			indices.append((x + 1) * line_fractions.y + y + x + 1)
-		indices.append(reset_index)
+	draw_fn = None
+	match draw_mode:
+		case DrawMode.TRIANGLE_STRIP_WITH_RESET_POINT:
+			index_count = (line_fractions.y + 1) * line_fractions.x * 2 + line_fractions.x
+			print(f"{index_count=}")
+			indices = gen_indices_triangle_strip_with_reset(line_fractions)
+			draw_fn = gl_draw_with_reset
+		case DrawMode.SEPARATE_TRIANGLES:
+			indices = gen_indices_separate_triangles(line_fractions)
+			draw_fn = gl_draw_triangles
+
 	print(f"{indices=}")
 	print(f"{len(indices)=}")
 
@@ -53,11 +85,30 @@ def generatePlane(screen, line_fractions: Point, edge_length: Point, starting_po
 		v_rect.center = v.as_tuple()
 		screen.blit(v_text, v_rect)
 
+	draw_fn(screen, vertices, indices)
+
+
+def gen_indices_triangle_strip_with_reset(line_fractions: Point) -> List[int]:
+	indices = []
+	for x in range(int(line_fractions.x)):
+		for y in range(int(line_fractions.y) + 1):
+			indices.append((x + 0) * line_fractions.y + y + x + 0)
+			indices.append((x + 1) * line_fractions.y + y + x + 1)
+		indices.append(GL_INDEX_RESET)
+	return indices
+
+
+def gen_indices_separate_triangles(line_fractions: Point) -> List[int]:
+	# TODO
+	return [1, 0, 6] + [6,7,1, 2,1,7, 7,8,2]
+
+
+def gl_draw_with_reset(screen, vertices: List[Point], indices: List[int]) -> None:
 	strips_indices = []
 	split_begin = 0
-	for i in indices:
+	for _ in indices:
 		try:
-			split_end = indices.index(-1, split_begin)
+			split_end = indices.index(GL_INDEX_RESET, split_begin)
 			strips_indices.append(indices[split_begin:split_end])
 			split_begin = split_end + 1
 		except ValueError:
@@ -70,6 +121,16 @@ def generatePlane(screen, line_fractions: Point, edge_length: Point, starting_po
 		c[1] = c[1] + delta
 		pygame.draw.lines(screen, c, False, [vertices[i].as_tuple() for i in strip_indices])
 
+
+def gl_draw_triangles(screen, vertices: List[Point], indices: List[int]) -> None:
+	c = [0, 0, 255]
+	for i in range(0, len(indices), 3):
+		t = Triangle(*map(lambda j: vertices[j], indices[i:i+3]))
+		t = t.scale(0.8)
+		print(f"{t=}")
+		pygame.draw.lines(screen, c, False, [p.as_tuple() for p in t.as_points_closed()])
+
+
 def main():
 	screen = pygame.display.set_mode((500, 500))
 	pygame.display.set_caption("terrain grid prototype")
@@ -79,7 +140,8 @@ def main():
 	line_fractions = Point(5, 5)
 	edge_length = Point(400, 400)
 	starting_point = Point(50, 50)
-	generatePlane(screen, line_fractions, edge_length, starting_point)
+	draw_mode = DrawMode.TRIANGLE_STRIP_WITH_RESET_POINT
+	generate_plane(screen, line_fractions, edge_length, starting_point, draw_mode)
 	pygame.display.flip()
 
 	keep_drawing = True
@@ -97,9 +159,15 @@ def main():
 					line_fractions.y = line_fractions.y + 1
 				if event.key == pygame.K_DOWN:
 					line_fractions.y = line_fractions.y - 1
+				if event.key == pygame.K_m:
+					print(f"b:{draw_mode=}")
+					draw_mode = DrawMode.SEPARATE_TRIANGLES if draw_mode == DrawMode.TRIANGLE_STRIP_WITH_RESET_POINT else DrawMode.TRIANGLE_STRIP_WITH_RESET_POINT
+					print(f"a:{draw_mode=}")
+
+				print(f"{draw_mode=}")
 
 				screen.fill((0, 0, 0))
-				generatePlane(screen, line_fractions, edge_length, starting_point)
+				generate_plane(screen, line_fractions, edge_length, starting_point, draw_mode)
 				pygame.display.flip()
 
 	print("bye")
